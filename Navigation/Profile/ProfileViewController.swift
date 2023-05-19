57435b67a37cc3a042e01bfa7d1b36479842a8b0
Vitali <i@borodulinvm.ru>
2023-05-19 15:07:17 +0300

    задание 2.2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -12,6 +12,12 @@ class ProfileViewController: UIViewController {
     
     let profileHeaderView = ProfileHeaderView()
 
+    var newButton: UIButton = {
+        var newButton = UIButton()
+        newButton.backgroundColor = .systemBlue
+        newButton.setTitle("newButton", for: .normal)
+        return newButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,18 +25,29 @@ class ProfileViewController: UIViewController {
         title = "Profile"
         view.addSubview(profileHeaderView)
         profileHeaderView.backgroundColor = .lightGray
+        view.addSubview(newButton)
+
         setConcstr()
+        setConstreintNewButton()
     }
+    
+    
+    
     func setConcstr() {
         profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
         profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         profileHeaderView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         profileHeaderView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        //profileHeaderView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         profileHeaderView.heightAnchor.constraint(equalToConstant: 220).isActive = true
     }
     
-
+    func setConstreintNewButton(){
+        newButton.translatesAutoresizingMaskIntoConstraints = false
+        newButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        newButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0 ).isActive = true
+        newButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+       }
     
    
 }
